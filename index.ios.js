be235db21be5d743d2c71d71f39a158656951376
antonyashyn <anya@grdnz.com>
2016-10-03 23:37:05 +0300

    fixed

--- index.ios.js
+++ index.ios.js
@@ -4,10 +4,10 @@ import React from 'react';
 import { AppRegistry, ScrollView, View, Dimensions, Text } from 'react-native';
 
 const screenWidth = Dimensions.get('window').width;
-const scrollViewWidth = screenWidth * 0.8;
-const cardWidth = scrollViewWidth * 0.84;
+const scrollViewWidth = Math.round(screenWidth * 0.90);
+const cardWidth = scrollViewWidth * 0.80;
 const paddingCard = scrollViewWidth * 0.02;
-const scrollViewPadding = scrollViewWidth * 0.04;
+const scrollViewPadding = scrollViewWidth * 0.08;
 
 const ScrollViewAlign = React.createClass({
   render() {
@@ -15,24 +15,24 @@ const ScrollViewAlign = React.createClass({
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <ScrollView
           contentContainerStyle={this.props.contentContainerStyle}
-          style={{flex: 0, backgroundColor: '#BDBDBD', height: screenWidth * 0.5, width: scrollViewWidth, paddingHorizontal: scrollViewPadding + paddingCard}}
+          style={{flex: 0, backgroundColor: '#BDBDBD', height: screenWidth * 0.5, width: scrollViewWidth}}
           snapToAlignment={'start'}
           contentOffset={{
             x: 0,
             y: 0,
           }}
-          snapToInterval={cardWidth + scrollViewPadding}
+          snapToInterval={cardWidth + paddingCard + paddingCard + 1}
           renderRow={this.renderRow}
           showsHorizontalScrollIndicator={false}
           horizontal={true}
         >
-          <View style={{backgroundColor: '#F44336', width: cardWidth, marginHorizontal: paddingCard}}><Text>First Card</Text></View>
+          <View style={{backgroundColor: '#F44336', width: cardWidth, marginLeft:paddingCard + scrollViewPadding, marginRight: paddingCard}}><Text>First Card</Text></View>
           <View style={{backgroundColor: '#9C27B0', width: cardWidth, marginHorizontal:paddingCard}}><Text>Second Card</Text></View>
           <View style={{backgroundColor: '#E91E63', width: cardWidth, marginHorizontal:paddingCard}}><Text>Third Card</Text></View>
           <View style={{backgroundColor: '#FF5722', width: cardWidth, marginHorizontal:paddingCard}}><Text>Third Card</Text></View>
           <View style={{backgroundColor: '#00E676', width: cardWidth, marginHorizontal:paddingCard}}><Text>Third Card</Text></View>
           <View style={{backgroundColor: '#00B0FF', width: cardWidth, marginHorizontal:paddingCard}}><Text>Third Card</Text></View>
-          <View style={{backgroundColor: '#1A237E', width: cardWidth, marginHorizontal: paddingCard}}><Text>Third Card</Text></View>
+          <View style={{backgroundColor: '#1A237E', width: cardWidth, marginRight:paddingCard + scrollViewPadding, marginLeft: paddingCard}}><Text>Third Card</Text></View>
         </ScrollView>
       </View>
     );
